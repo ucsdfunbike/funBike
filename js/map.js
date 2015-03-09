5d7945f727c2a728a532ecaaa23e52072054013b
Shaina Wan <s1wan@ucsd.edu>
2015-03-08 20:10:30 -0700

    Fixed UI and updated the google maps api

--- js/map.js
+++ js/map.js
@@ -3,12 +3,26 @@ var map;
 //Places map on the page
 function initialize() {
   var mapProp = {
-    center:new google.maps.LatLng(40,0),
+    center: new google.maps.LatLng(40,0),
     zoom:2,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+    streetViewControl: true,
+    mapTypeId:google.maps.MapTypeId.ROADMAP,
+    /*mapTypeControlOptions : {
+      mapTypeIds: [
+        google.maps.MapTypeId.SATELLITE
+      ]
+    }*/
   };
   //Creates new google map and places on the webpage
   map = new google.maps.Map(document.getElementById("map-canvas"),mapProp);
+  //Famous Landmarks
+  /*
+  var goldenGatePark = new google.maps.LatLng(37.768837,-122.483412);
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var marker = new google.maps.Marker({
+      position: chicago,
+      map: map
+  });*/
 }
 //Event Listener to load the map
 google.maps.event.addDomListener(window, 'load', initialize);
